127e5b6ee79ec506fe2ccc9f407d62fa03ba856c
Adrian Eduardo <adrian.furquim26@gmail.com>
2023-08-18 14:51:20 -0300

    Insesrindo o spring no git

---
+++ src/main/java/edu/ifsp/connectdengue/connectdengueapi/ConnectdengueApiApplication.java
@@ -0,0 +1,13 @@
+package edu.ifsp.connectdengue.connectdengueapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectdengueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectdengueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ifsp/connectdengue/connectdengueapi/ConnectdengueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ifsp.connectdengue.connectdengueapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectdengueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
